26e5d685a02b1a972e85f0f94392af3681701690
Samuel Arenas <96198750+samuelfsl@users.noreply.github.com>
2022-11-28 17:54:57 -0500

    Updates

--- src/components/About/About.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import About from './About';
-
-test('renders the about component', () => {
-  render(<About />);
-  const title = screen.getByText(/Como comprar/);
-  expect(title).toBeInTheDocument();
-});
--- src/components/Header/Header.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Header from './Header';
-
-test('renders the header', () => {
-  render(<Header />);
-  const header = screen.getByText('Ver productos');
-  expect(header).toBeInTheDocument();
-});
